91eed3bb9a2118fd8e0ea256b69fc49ed4abf1ff
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-11 23:00:46 -0300

    test: seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,6 +17,54 @@ async function main() {
   }
 
   console.log({ event });
+
+  await prisma.ticketType.deleteMany({});
+  await prisma.ticketType.createMany({data: [
+    {
+      name: 'P100',
+      price: 50000,
+      isRemote: false,
+      includesHotel: true,
+    },
+    {
+      name: 'O100',
+      price: 20000,
+      isRemote: true,
+      includesHotel: false,
+    },
+  ]});
+  
+  await prisma.room.deleteMany({});
+  await prisma.hotel.deleteMany({});
+  const hotel1 = await prisma.hotel.create({ data: { name: 'Driven Resort', image: 'https://cdn.pixabay.com/photo/2017/01/28/19/31/landscape-2016308_640.jpg'}});
+  const hotel2 = await prisma.hotel.create({ data: { name: 'Driven Palace', image: 'https://cdn.pixabay.com/photo/2014/09/26/04/54/holiday-complex-461628_640.jpg'}});
+  const hotel3 = await prisma.hotel.create({ data: { name: 'Driven World', image: 'https://cdn.pixabay.com/photo/2017/12/16/22/22/bora-bora-3023437_640.jpg'}});
+  
+  await prisma.room.createMany({
+    data: [
+      { name: 'R101', capacity: 3, hotelId: hotel1.id},
+      { name: 'R102', capacity: 2, hotelId: hotel1.id},
+      { name: 'R103', capacity: 1, hotelId: hotel1.id},
+      { name: 'R104', capacity: 4, hotelId: hotel1.id},
+      { name: 'R105', capacity: 2, hotelId: hotel1.id},
+    ]
+  });
+  await prisma.room.createMany({
+    data: [
+      { name: 'P101', capacity: 3, hotelId: hotel2.id},
+      { name: 'P102', capacity: 2, hotelId: hotel2.id},
+      { name: 'P104', capacity: 4, hotelId: hotel2.id},
+      { name: 'P105', capacity: 2, hotelId: hotel2.id},
+    ]
+  });
+  await prisma.room.createMany({
+    data: [
+      { name: 'W101', capacity: 3, hotelId: hotel3.id},
+      { name: 'W102', capacity: 2, hotelId: hotel3.id},
+      { name: 'W103', capacity: 1, hotelId: hotel3.id},
+      { name: 'W105', capacity: 2, hotelId: hotel3.id},
+    ]
+  });
 }
 
 main()
