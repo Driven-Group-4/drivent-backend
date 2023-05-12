eea3f455371b471f6e424de545b9f2f964bb4f09
Mikael Josias <mikaeljosias2002@gmail.com>
2023-05-11 22:55:47 -0300

    feat: get hotels with rooms info route

--- src/routers/hotels-router.ts
+++ src/routers/hotels-router.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { authenticateToken } from '@/middlewares';
-import { getHotels, getHotelsWithRooms } from '@/controllers/hotel-controller';
+import { getHotels, getHotelsWithRooms, getHotelsWithRoomsInfo } from '@/controllers/hotel-controller';
 
 const hotelsRouter = Router();
 
-hotelsRouter.all('/*', authenticateToken).get('/', getHotels).get('/:hotelId', getHotelsWithRooms);
+hotelsRouter
+  .all('/*', authenticateToken)
+  .get('/', getHotels)
+  .get('/rooms', getHotelsWithRoomsInfo)
+  .get('/:hotelId', getHotelsWithRooms);
 
 export { hotelsRouter };
