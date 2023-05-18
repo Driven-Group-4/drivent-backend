7e5aa4e494ee52ea4b13282119c73774ffaa74a6
JeffMota <jmota906@gmail.com>
2023-05-18 16:50:07 -0300

    refactor: redis on event config

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -1,7 +1,18 @@
-import { prisma } from '@/config';
+import { prisma, redis } from '@/config';
 
 async function findFirst() {
-  return prisma.event.findFirst();
+  const cacheKey = 'event';
+  const cachedEvent = await redis.get(cacheKey);
+  if (cachedEvent) {
+    const event = JSON.parse(cachedEvent);
+    return event;
+  }
+
+  const event = await prisma.event.findFirst();
+
+  redis.setEx(cacheKey, 3, JSON.stringify(event));
+
+  return event;
 }
 
 const eventRepository = {
