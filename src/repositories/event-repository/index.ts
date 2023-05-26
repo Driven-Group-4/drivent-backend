06b57451bc5207501cf13c2f3d96394109dd6d41
JeffMota <jmota906@gmail.com>
2023-05-26 10:44:18 -0300

    bugFix:bug redis test

--- src/repositories/event-repository/index.ts
+++ src/repositories/event-repository/index.ts
@@ -2,10 +2,12 @@ import { prisma, redis } from '@/config';
 
 async function findFirst() {
   const cacheKey = 'event';
-  const cachedEvent = await redis.get(cacheKey);
-  if (cachedEvent) {
-    const event = JSON.parse(cachedEvent);
-    return event;
+  if (process.env.NODE_ENV !== 'test') {
+    const cachedEvent = await redis.get(cacheKey);
+    if (cachedEvent) {
+      const event = JSON.parse(cachedEvent);
+      return event;
+    }
   }
 
   const event = await prisma.event.findFirst();
